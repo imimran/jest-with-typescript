ea50d3ab2363e92baa8aa1d6c72c553117b174fd
imimran <engr.aih@gmail.com>
2021-11-24 18:44:24 +0600

    test case for remove image

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -34,19 +34,9 @@ describe("Test the root path", () => {
   });
 });
 
-// // Async, await way
-// describe("It shuld be get methoad", () => {
-//   test("get all image url", async () => {
-//     const res = await request(app).get("/api/v1/all");
-//     expect(res.statusCode).toBe(200);
-//     // console.log(res.body);
-//     let images = res.body
-//     // expect(images.length).toBeGreaterThan(0);
-//     expect(images.length).toBe(10);
-//   });
-// });
 
-describe("It shuld be get methoad", () => {
+describe("Image CRUD operation test", () => {
+  //For mocking db
   beforeAll(async () => {
     const mongoServer = await MongoMemoryServer.create();
 
@@ -58,6 +48,7 @@ describe("It shuld be get methoad", () => {
     await mongoose.connection.close();
   });
 
+
   it("should return a 200 & get image by id", async () => {
     const image = await Image.create(fakeImage);
     console.log("image", image);
@@ -65,20 +56,31 @@ describe("It shuld be get methoad", () => {
     const { body, statusCode } = await request(app).get(
       `/api/v1/image/${image._id}`
     );
-
-    console.log("body", body);
-
+    // console.log("body", body);
     expect(statusCode).toBe(200);
-
     expect(body.data.title).toBe(image.title);
   });
+
 
   it("Should return a 201 & create a image", async () => {
     const { body, statusCode } = await request(app)
       .post(`/api/v1/add-image-url`)
       .send(fakeAddImage);
-
     expect(statusCode).toBe(201);
     expect(body.data.imageFullURL).toEqual( fakeAddImage.imageFullURL);
   });
+
+
+  it("Should return a 200 & delete a image", async () => {
+    const image = await Image.create(fakeAddImage);
+    console.log("image", image);
+    const { body, statusCode } = await request(app).delete(
+      `/api/v1/remove-image/${image._id}`
+    );
+    console.log("body", body);
+    expect(statusCode).toBe(200);
+    expect(body.msg).toEqual('Delete Successfuly');
+  });
+
+  
 });
