075a8a140bc9a575d6cba92f0413e25725173437
imimran <engr.aih@gmail.com>
2021-11-25 18:57:36 +0600

    jest.fn()

--- __tests__/image.test.ts
+++ __tests__/image.test.ts
@@ -93,6 +93,34 @@ describe("Image CRUD operation test", () => {
   
 });
 
+describe.only("mock function", () =>{
+  it("returns undefined and has been called correct number of times", () => {
+      const mock = jest.fn();
+    
+      const result = mock();
+    
+      expect(result).toBeUndefined();
+      expect(mock).toHaveBeenCalledTimes(1);
+      expect(mock).toHaveBeenCalledWith();
+    });
+    
+    it("has been called with correct arguments and returns a correct value", () => {
+      const mock = jest
+        .fn()
+        .mockReturnValueOnce("first return")
+        .mockReturnValueOnce("second return");
+    
+      const resultFirst = mock("first call");
+      const resultSecond = mock("second call");
+    
+      expect(resultFirst).toBe("first return");
+      expect(resultSecond).toBe("second return");
+      expect(mock).toHaveBeenCalledTimes(2);
+      expect(mock).toHaveBeenNthCalledWith(1, "first call");
+      expect(mock).toHaveBeenNthCalledWith(2, "second call");
+    });
+})
+
 
 
 // import request from "supertest";
